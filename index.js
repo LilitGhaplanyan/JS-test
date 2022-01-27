1de435d2f2cf0c1fdb6908cb4eb48bb1f481fb6b
Lilit <lilit8708@gmail.com>
2022-01-27 05:31:49 +0400

    feat(winner): doesCartWinPrize

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const WINNING_SUM = 123.40;
 @param {Array} cartItemsPrices
 */
 function doesCartWinPrize(cartItemsPrices) {
-  
+    const newCartItemsPrices = cartItemsPrices.map(Number)
+    let result = newCartItemsPrices.reduce((sum, current) => sum + current, 0)
+    const sum = Number(result.toFixed(2))
+    if (sum === WINNING_SUM) {
+        return true
+    } else {
+        return false
+    }
 }
 
 module.exports = doesCartWinPrize;
+
